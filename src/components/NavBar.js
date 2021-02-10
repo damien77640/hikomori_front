b849e908f8a7bb3e8ed5325c76bdc1eb5cb431e7
A.OULD <72967617+Adom7@users.noreply.github.com>
2021-02-10 12:04:27 +0100

    NavBar done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,31 @@
 import React from "react"
 import { Nav, NavDropdown, Navbar, Form, FormControl, Button} from 'react-bootstrap';
+import HikomoriWhiteLogo from "../image/HikomoriLogoWhite.png"
 
 function NavBar(){
     return(
       <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Navbar.Brand href="/home">
+      <img
+        src= {HikomoriWhiteLogo}
+        width="200"
+        height="50"
+        className="d-inline-block align-top"
+        alt="Hikomori White logo"
+      />
+    </Navbar.Brand>
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="/favorites">Favorites</Nav.Link>
+      <Nav.Link href="/forum">Forum</Nav.Link>
+      <Nav.Link href="/maps">Maps</Nav.Link>
     </Nav>
     <Form inline>
       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
       <Button variant="outline-info">Search</Button>
+
     </Form>
   </Navbar>
     )
         }
-
-
-
+        
 export default NavBar
--- src/pages/DashboardTest.js
+++ src/pages/DashboardTest.js
@@ -1,13 +1,15 @@
 import React from "react"
 import Navbar from "../components/NavBar.js"
+import Container from "../components/Container.js"
+
 
 
 
 function DashboardTest() {
     return (
       <div className="Dashboard">
-        <Navbar />    
-     </div>
+        <Navbar /> 
+        </div>
     );
   }
 
