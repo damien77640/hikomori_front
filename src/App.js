35d3b0ecf6d2391a5160662658067d7ae82eae37
dada <damien.dylan77@gmail.com>
2021-02-10 15:02:52 +0100

    navbar test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Switch, Route} from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import DashboardTest from "./pages/DashboardTest";
+import NavbarTest from "./pages/NavbarTest";
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
         <Switch>
             <Route exact path="/" render = {props => (<Dashboard {...props}/>)} />
             <Route exact path="/home" render = {props => (<DashboardTest {...props}/>)} />
+            <Route exact path="/manga" render = {props => (<NavbarTest {...props}/>)} />
         </Switch>
       </BrowserRouter>
   );
