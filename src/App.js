5f3014ace6a5d6a91bd8e1f4116473d651956516
Soufian LABED <labedsoufian@gmail.com>
2021-02-10 14:19:35 +0100

    manga_detail

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Switch, Route} from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import DashboardTest from "./pages/DashboardTest";
+import Detail_manga from "./pages/Detail_manga";
+
 
 
 const App = () => {
@@ -10,6 +12,7 @@ const App = () => {
         <Switch>
             <Route exact path="/" render = {props => (<Dashboard {...props}/>)} />
             <Route exact path="/home" render = {props => (<DashboardTest {...props}/>)} />
+            <Route exact path="/manga/:id" component={Detail_manga}/>
         </Switch>
       </BrowserRouter>
   );
---
+++ src/pages/Detail_manga.js
@@ -0,0 +1,132 @@
+import React from "react"
+import Navbar from "../components/NavBar.js";
+import './Detail_manga.css';
+import SimpleRating from '../components/SimpleRating';
+import NavBar from '../components/NavBar'
+
+//import {Link} from 'react-router-dom';
+
+
+
+
+
+
+class Detail_manga extends React.Component{
+
+  
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+        id : this.props.match.params.id,
+    }
+
+    
+}
+
+
+
+
+
+
+async componentDidMount(){
+
+  // je vais chercher l'ensemble des info du pokemon correspondant à l'id 
+  const response = await fetch(`http://localhost:5000/manga/${this.state.id}`)
+
+ 
+
+
+  
+
+  const data = await response.json()
+
+  console.log(data)
+  
+
+
+  
+  this.setState({manga : data})
+  
+ 
+
+  
+}
+
+
+
+  render(){
+
+    
+
+    return (
+        
+        <div className="all_page" >
+            <NavBar/>
+          
+        {this.state.manga ? (
+
+          <div >
+
+               
+
+            {this.state.manga.map(manga => (
+           
+                <div>
+                <div className="manga_info" style={{color:"black"}}>
+               
+                           
+                
+                           <img className="img_manga" style={{float:"left",borderRadius:"13px"}} src={manga.posterImageSmall} alt="Logo" />
+                           <div className="info_droite">
+                            
+                           <h1> {manga.tittles_en} </h1> 
+                           <SimpleRating/>
+                           </div>
+
+                           
+                
+                    
+                </div>
+
+                <div>
+                <h2>SYNOPSIS</h2>
+
+                <div className="synopsis">
+                    {manga.synopsis}
+                </div>
+                </div>
+
+                </div>
+                                    
+
+                    
+                    
+         
+              
+
+             
+            ))}
+          </div>
+        ) : (
+         <div> Erreur </div>
+        )}
+
+
+      
+        
+
+
+        </div>
+      
+    );
+  }
+
+
+
+
+
+
+  }
+export default Detail_manga;
