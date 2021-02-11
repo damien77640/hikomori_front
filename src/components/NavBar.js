cb012a4709c248c8c8986d01d94a3b5018df7ae7
dada <damien.dylan77@gmail.com>
2021-02-11 09:48:36 +0100

    navbar operationnel

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -99,7 +99,7 @@ const useStyles = makeStyles((theme) => ({
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing(3),
+    
     transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
